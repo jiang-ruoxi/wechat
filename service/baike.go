d4ebe1a8e6784784920f1caf4afcd7a875c5cdf2
xiaomi <xiaomi@b9.com>
2023-05-06 09:47:29 +0800

    修

--- service/baike.go
+++ service/baike.go
@@ -73,6 +73,7 @@ func (bs *BaiKeService) SendMsg() {
 
 	// 将 body 数据转为 json
 	requestBodyJson, err := json.Marshal(requestBody)
+	log.Printf("请求参数:%#v \n", requestBodyJson)
 	if err != nil {
 		fmt.Println("json.Marshal error:", err)
 		return
