780b64097bb508c49759834f93cbd98cabf9a559
xiaomi <xiaomi@b9.com>
2023-06-02 22:32:38 +0800

    西

--- app/huiben.go
+++ app/huiben.go
@@ -15,16 +15,18 @@ func AddVideoLog(c *gin.Context) {
 
 func UploadMp3(c *gin.Context)  {
 	file, err := c.FormFile("file")
-	global.WECHAT_LOG.Info(fmt.Sprintf("上传录音接收到的参数：%#v \n", file))
+	fmt.Printf("上传录音接收到的参数:%s \n",file)
 	if err == nil {
 		var Path string = "/data/web/static/video"
 		dst := path.Join(Path, file.Filename)
-		c.SaveUploadedFile(file, dst)
+		e := c.SaveUploadedFile(file, dst)
+		fmt.Printf("SaveUploadedFile:%s \n",e)
 		dst = strings.Replace(dst, Path, "https://static.58haha.com/video", 1)
 		c.JSON(200, gin.H{
 			"dst": dst,
 		})
 	} else {
+		fmt.Println("UploadMp3失败")
 		global.WECHAT_LOG.Info(fmt.Sprintf("AddUploads：%#v \n", err))
 		common.ReturnResponse(common.FAIL, map[string]interface{}{}, common.FAIL_MSG, c)
 	}
