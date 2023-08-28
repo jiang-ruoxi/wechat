e302ffedbe91317fa0949215cd8b53fd40fd421a
若熙~ <oncwnuE9cvc3j9g4Yk0AFSNyIEF0@git.weixin.qq.com>
2023-08-28 10:20:40 +0800

    删除路由

--- router/router.go
+++ router/router.go
@@ -27,29 +27,6 @@ func InitRouter() *gin.Engine {
 		api.GET("/poetry/school/getPoetryLog", app.ApiPoetryLog)
 		api.POST("/poetry/school/uploadMp3", app.ApiUploadPoetryMp3)
 		api.POST("/poetry/school/addVideoLog", app.ApiAddPoetryVideoLog)
-		//初高中
-		api.GET("/poetry/junior/getList", routerCache(global.RedisURL_CACHE), app.ApiJuniorPoetryList)
-		api.GET("/poetry/junior/getPoetryInfo", routerCache(global.RedisURL_CACHE), app.ApiJuniorPoetryInfo)
-		//成语
-		api.GET("/poetry/cheng/getList", routerCache(global.RedisURL_CACHE), app.ApiChengPoetryList)
-		api.GET("/poetry/cheng/getPoetryInfo", routerCache(global.RedisURL_CACHE), app.ApiChengPoetryInfo)
-
-		//中文绘本
-		api.GET("/chinese/getList", routerCache(global.RedisURL_CACHE), app.ApiChineseBookList)
-		api.GET("/chinese/getBookInfo", routerCache(global.RedisURL_CACHE), app.ApiChineseBookInfo)
-
-		//古诗绘本
-		api.GET("/poetry/book/getList", routerCache(global.RedisURL_CACHE), app.ApiPoetryBookList)
-		api.GET("/poetry/book/getBookInfo", routerCache(global.RedisURL_CACHE), app.ApiPoetryBookInfo)
-
-		//英文绘本
-		api.GET("/english/getList", routerCache(global.RedisURL_CACHE), app.ApiEnglishBookList)
-		api.GET("/english/getBookInfo", routerCache(global.RedisURL_CACHE), app.ApiEnglishBookInfo)
-		api.GET("/english/getOpenId", app.ApiOpenId)
-
-		//百科知识
-		api.GET("/baike/getCategoryCount", routerCache(global.RedisURL_CACHE), app.ApiCategoryCount)
-		api.GET("/baike/getQuestion", app.ApiQuestion)
 
 		//下载图片
 		api.GET("/downLoad/pic", app.ApiDownLoadPic)
